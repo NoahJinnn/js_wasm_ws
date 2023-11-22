d0ebb8322a14e41d01fa8c5f402245bfd9d1953f
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-11-22 21:40:32 +0700

    Handle on message, add mpsc channel to send out message

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,33 @@ pub fn ws_ping(endpoint: String, message: String) -> Promise {
     }) as Box<dyn FnMut(JsValue)>);
     ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
     onopen_callback.forget();
+
+    // On message, log it and send message to channel
+    let (sender, mut receiver) = mpsc::unbounded::<JsValue>();
+    // create callback
+    let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+        // handle message
+        let response = match e.data().as_string() {
+            Some(data) => data,
+            None => {
+                console_log!("error converting data to string");
+                return;
+            }
+        };
+        console_log!("message event, received data: {:?}", response);
+
+        match sender.unbounded_send(e.data()) {
+            Ok(_) => console_log!("channel sent success"),
+            Err(err) => console_log!("channel send fail: {:?}", err),
+        };
+    }) as Box<dyn FnMut(MessageEvent)>);
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
+
+    // On receive message from channel, return it to JS
+    future_to_promise(async move {
+        match receiver.next().await {
+            Some(value) => Ok(value),
+            None => Err(JsValue::from_str("No response received")),
+        }
+    })
